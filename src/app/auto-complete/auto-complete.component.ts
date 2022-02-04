fec38fa50460c7284efe0c87f8090f87ff13394a
Kevin Logan <kevin.logan@ravenind.com>
2022-02-04 10:50:22 -0600

    filtering teams and default works

--- src/app/auto-complete/auto-complete.component.ts
+++ src/app/auto-complete/auto-complete.component.ts
@@ -1,10 +1,15 @@
 // https://www.itsolutionstuff.com/post/angular-material-autocomplete-with-api-exampleexample.html
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+import { Form, FormControl } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { concatAll, debounceTime, defaultIfEmpty, distinctUntilChanged, filter, map, startWith, tap } from 'rxjs/operators';
 
-export interface User {
+export interface TeamMember {
+  name: string;
+  team: string;
+}
+
+export interface Team {
   name: string;
 }
 @Component({
@@ -13,31 +18,58 @@ export interface User {
   styleUrls: ['./auto-complete.component.css']
 })
 export class AutoCompleteComponent implements OnInit {
-  myControl = new FormControl();
-  options: User[] = [{name: 'Mary'}, {name: 'Shelley'}, {name: 'Igor'}];
-  filteredOptions!: Observable<User[]>;
-  selectedUser!: User;
+  teams: Team[] = [{ name: 'Vikings' }, { name: 'Bears' }, { name: 'Packers' }, { name: 'Lions' }];
+  filteredTeams!: Observable<Team[]>;
+  teamMembers: TeamMember[] = [
+    { name: 'Chris Carter', team: 'Vikings' },
+    { name: 'Brett Favre', team: 'Packers' },
+    { name: 'Brian Urlacher', team: 'Bears' },
+    { name: 'Barry Sanders', team: 'Lions' }];
+  filteredTeamMembers!: Observable<TeamMember[]>;
+  selectedTeam = this.teams[0];
+  selectedTeamMember!: TeamMember;
+  teamControl = new FormControl(this.selectedTeam);
+  teamMemberControl = new FormControl();
 
+  constructor() { }
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => (typeof value === 'string' ? value : value.name)),
-      map(name => (name ? this._filter(name) : this.options.slice())),
+    this.filteredTeams = this.teamControl.valueChanges.pipe(
+      tap(e => console.log('team start' + e)),
+      debounceTime(400),
+      distinctUntilChanged(),
+      // on select, you get a full object, on type you get a string
+      map((value: string | Team) => (typeof value === 'string' ? value : value.name)),
+      map(v => (v ? this._filterTeams(v) : this.teams.slice())),
+      tap(e => e.forEach(f => console.log('team ' + f.name))),
+    );
+    this.filteredTeamMembers = this.teamMemberControl.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      map((value: string | TeamMember) => (typeof value === 'string' ? value : value.name)),
+      map(v => (v ? this._filterTeamMembers(v) : this.teamMembers.slice())),
+      tap(e => e.forEach(f => console.log(`member: ${f.name}`)))
     );
   }
 
-  displayFn(user: User): string {
+  teamDisplayFn(team: Team): string {
+    return team && team.name ? team.name : '';
+  }
+
+  private _filterTeams(name: string): Team[] {
+    return this.teams.filter(option => option.name.toLowerCase().includes(name.toLowerCase()));
+  }
+
+  private _filterTeamMembers(name: string): TeamMember[] {
+    let byTeam = this.teamMembers.filter(t => t.team === this.selectedTeam.name);
+    return byTeam.filter(option => option.name.toLowerCase().includes(name.toLowerCase()));
+  }
+
+  teamMemberDisplay(user: TeamMember): string {
     return user && user.name ? user.name : '';
   }
 
-  private _filter(name: string): User[] {
-    const filterValue = name.toLowerCase();
-
-    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
-  }
-
-  onSelect(option: User){
-    this.selectedUser = option;
+  onTeamMemberSelect(option: TeamMember) {
+    this.selectedTeamMember = option;
   }
 
 }
